e5f0e9ced84a3cbca91eeb715c059a6d95482e53
unknown <zelto@hotmail.com>
2023-09-11 17:40:17 -0300

    implementa fluxo controller e cria service

--- src/main/java/com/uzitec/clienteservico/application/api/ClienteRestContoller.java
+++ src/main/java/com/uzitec/clienteservico/application/api/ClienteRestContoller.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.uzitec.clienteservico.application.api.request.ClienteRequest;
 import com.uzitec.clienteservico.application.api.response.ClienteResponse;
+import com.uzitec.clienteservico.application.service.ClienteService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,11 +15,14 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class ClienteRestContoller implements ClienteAPI {
 
+	private final ClienteService clienteService;
+
 	@Override
 	public ClienteResponse postCliente(@Valid ClienteRequest clienteRequest) {
 		log.info("[inicia] ClienteRestContoller - postCliente");
+		ClienteResponse clienteCriado = clienteService.criaCliente(clienteRequest);
 		log.info("[finaliza] ClienteRestContoller - postCliente");
-		return null;
+		return clienteCriado;
 	}
 
 }
