2fda89ea760932e717eee59e5b1dd00833190fdd
unknown <zelto@hotmail.com>
2023-09-11 19:23:58 -0300

    implementa contrato da API, getClientePorId

--- src/main/java/com/uzitec/clienteservico/application/api/ClienteAPI.java
+++ src/main/java/com/uzitec/clienteservico/application/api/ClienteAPI.java
@@ -1,6 +1,7 @@
 package com.uzitec.clienteservico.application.api;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uzitec.clienteservico.application.api.request.ClienteRequest;
-import com.uzitec.clienteservico.application.api.response.ClienteResponse;
+import com.uzitec.clienteservico.application.api.response.ClienteDetalhadoResponse;
 import com.uzitec.clienteservico.application.api.response.ClienteListResponse;
+import com.uzitec.clienteservico.application.api.response.ClienteResponse;
 
 import jakarta.validation.Valid;
 
@@ -27,4 +29,9 @@ public interface ClienteAPI {
 	@GetMapping
 	@ResponseStatus(code = HttpStatus.OK)
 	List<ClienteListResponse> getTodosClientes();
+	
+	@GetMapping(value = "/{idCliente")
+	@ResponseStatus(code = HttpStatus.OK)
+	ClienteDetalhadoResponse getClientePorId(@Valid @RequestBody UUID idCliente);;
+	
 }
