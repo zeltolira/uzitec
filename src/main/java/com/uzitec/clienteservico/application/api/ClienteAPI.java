0bf47eb52cfd08deda82b85b7474be597c4cf8fc
unknown <zelto@hotmail.com>
2023-09-11 18:27:37 -0300

    implementa getTodosUsuarios e cria ClienteListResponse

--- src/main/java/com/uzitec/clienteservico/application/api/ClienteAPI.java
+++ src/main/java/com/uzitec/clienteservico/application/api/ClienteAPI.java
@@ -1,6 +1,9 @@
 package com.uzitec.clienteservico.application.api;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.uzitec.clienteservico.application.api.request.ClienteRequest;
 import com.uzitec.clienteservico.application.api.response.ClienteResponse;
+import com.uzitec.clienteservico.application.api.response.ClinteListResponse;
 
 import jakarta.validation.Valid;
 
@@ -19,4 +23,8 @@ public interface ClienteAPI {
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	ClienteResponse postCliente(@Valid @RequestBody ClienteRequest clienteRequest);
+	
+	@GetMapping
+	@ResponseStatus(code = HttpStatus.OK)
+	List<ClinteListResponse> getTodosClientes();
 }
---
+++ src/main/java/com/uzitec/clienteservico/application/api/response/ClinteListResponse.java
@@ -0,0 +1,18 @@
+package com.uzitec.clienteservico.application.api.response;
+
+import com.uzitec.clienteservico.domain.Sexo;
+
+import lombok.Value;
+
+@Value
+public class ClinteListResponse {
+	private String nomeCliente;
+	private Sexo sexo;
+	private String telefone;
+	private String rua;
+	private String bairro;
+	private String numeroDaCasa;
+	private String cidade;
+	private String cpf;
+	private String rg;
+}
