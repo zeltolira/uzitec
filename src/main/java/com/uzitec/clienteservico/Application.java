e18e68f7da7b5dd6a0838a23af312d3b996bf90c
unknown <zelto@hotmail.com>
2023-09-11 14:03:08 -0300

    configura banco de dados e testa aplicacao web. teste ok!

--- src/main/java/com/uzitec/clienteservico/Application.java
+++ src/main/java/com/uzitec/clienteservico/Application.java
@@ -2,10 +2,19 @@ package com.uzitec.clienteservico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class Application {
 
+	@GetMapping
+	public String getHomeTest() {
+		return "Uzitec Concerto de máquinas de lavar, há mais de 20 anos no mercado fazendo o melhor para você!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
