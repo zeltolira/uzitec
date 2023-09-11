a0e42588a24c41c2624b2f5eb5e915a08d73c3b1
unknown <zelto@hotmail.com>
2023-09-11 19:27:26 -0300

    implementa cadama controller e service

--- src/main/java/com/uzitec/clienteservico/application/api/ClienteRestContoller.java
+++ src/main/java/com/uzitec/clienteservico/application/api/ClienteRestContoller.java
@@ -1,14 +1,17 @@
 package com.uzitec.clienteservico.application.api;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uzitec.clienteservico.application.api.request.ClienteRequest;
+import com.uzitec.clienteservico.application.api.response.ClienteDetalhadoResponse;
 import com.uzitec.clienteservico.application.api.response.ClienteListResponse;
 import com.uzitec.clienteservico.application.api.response.ClienteResponse;
 import com.uzitec.clienteservico.application.service.ClienteService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -33,6 +36,14 @@ public class ClienteRestContoller implements ClienteAPI {
 		List<ClienteListResponse>  clientes = clienteService.buscaTodosClientes();
 		log.info("[finlaiza] ClienteRestContoller - getTodosClientes");
 		return clientes;
+	}
+
+	@Override
+	public ClienteDetalhadoResponse getClientePorId(@Valid UUID idCliente) {
+		log.info("[inicia] ClienteRestContoller - getClientePorId");
+		ClienteDetalhadoResponse clienteDetalhado = clienteService.buscaClientesPorId(idCliente);
+		log.info("[finaliza] ClienteRestContoller - getClientePorId");
+		return clienteDetalhado;
 	}
 
 }
--- src/main/java/com/uzitec/clienteservico/application/service/ClienteService.java
+++ src/main/java/com/uzitec/clienteservico/application/service/ClienteService.java
@@ -1,12 +1,17 @@
 package com.uzitec.clienteservico.application.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.uzitec.clienteservico.application.api.request.ClienteRequest;
+import com.uzitec.clienteservico.application.api.response.ClienteDetalhadoResponse;
 import com.uzitec.clienteservico.application.api.response.ClienteListResponse;
 import com.uzitec.clienteservico.application.api.response.ClienteResponse;
+
+import jakarta.validation.Valid;
 
 public interface ClienteService {
 	ClienteResponse criaCliente(ClienteRequest clienteRequest);
 	List<ClienteListResponse> buscaTodosClientes();
+	ClienteDetalhadoResponse buscaClientesPorId(@Valid UUID idCliente);
 }
