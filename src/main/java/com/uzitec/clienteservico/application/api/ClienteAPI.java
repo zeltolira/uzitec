e33826b5797fed2d5a9215bfc081ec6895c61b5f
unknown <zelto@hotmail.com>
2023-09-12 13:12:36 -0300

    implementa delete na api

--- src/main/java/com/uzitec/clienteservico/application/api/ClienteAPI.java
+++ src/main/java/com/uzitec/clienteservico/application/api/ClienteAPI.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,4 +42,7 @@ public interface ClienteAPI {
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	void alteraClientePorId(@PathVariable UUID idCliente, @Valid @RequestBody ClienteAlteracaoRequest clienteAlteracaoRequest);
 		
+	@DeleteMapping(value = "/{idCliente}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	void deletaClientePorId(@PathVariable UUID idCliente);
 }
