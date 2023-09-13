4c70a6079e1b3a1a2bafdcb98c353d140b364280
unknown <zelto@hotmail.com>
2023-09-13 06:48:10 -0300

    implementa request e response

--- src/main/java/com/uzitec/clienteservico/funcionario/application/api/request/FuncionarioRequest.java
+++ src/main/java/com/uzitec/clienteservico/funcionario/application/api/request/FuncionarioRequest.java
@@ -1,5 +1,24 @@
 package com.uzitec.clienteservico.funcionario.application.api.request;
 
-public class FuncionarioRequest {
+import org.hibernate.validator.constraints.br.CPF;
 
+import com.uzitec.clienteservico.funcionario.domain.Cargo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+@Value
+public class FuncionarioRequest {
+	@NotBlank(message = "Campo Obrigatório")
+	private String nomeFuncionario;
+	@NotBlank(message = "Campo Obrigatório")
+	@CPF
+	@Column(unique = true)
+	private String cpf;
+	private String celular;
+	@Enumerated(EnumType.STRING)
+	private Cargo cargo;
 }
--- src/main/java/com/uzitec/clienteservico/funcionario/application/api/response/FuncionarioResponse.java
+++ src/main/java/com/uzitec/clienteservico/funcionario/application/api/response/FuncionarioResponse.java
@@ -1,5 +1,25 @@
 package com.uzitec.clienteservico.funcionario.application.api.response;
 
-public class FuncionarioResponse {
+import java.util.UUID;
 
+import com.uzitec.clienteservico.funcionario.domain.Cargo;
+import com.uzitec.clienteservico.funcionario.domain.Funcionario;
+
+import lombok.Value;
+
+@Value
+public class FuncionarioResponse {
+	private UUID idFuncionario;
+	private String nomeFuncionario;
+	private String cpf;
+	private String celular;
+	private Cargo cargo;
+
+	public FuncionarioResponse(Funcionario funcionario) {
+		this.idFuncionario = funcionario.getIdFuncionario();
+		this.nomeFuncionario = funcionario.getNomeFuncionario();
+		this.cpf = funcionario.getCpf();
+		this.celular = funcionario.getCelular();
+		this.cargo = funcionario.getCargo();
+	}
 }
