f449f9aab3d68e5e452feaab4b849f5c5fdb324b
unknown <zelto@hotmail.com>
2023-09-14 13:16:56 -0300

    implementa alteraFuncionario no contrato da api e cria alteraFuncionario

--- src/main/java/com/uzitec/clienteservico/funcionario/application/api/FuncionarioAPI.java
+++ src/main/java/com/uzitec/clienteservico/funcionario/application/api/FuncionarioAPI.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.uzitec.clienteservico.funcionario.application.api.request.FuncionarioRequest;
+import com.uzitec.clienteservico.funcionario.application.api.request.alteraFuncionario;
 import com.uzitec.clienteservico.funcionario.application.api.response.FuncionarioResponse;
 
 import jakarta.validation.Valid;
@@ -35,5 +37,9 @@ public interface FuncionarioAPI {
 	@DeleteMapping(value = "/{idFuncionario}")
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	void deletaFuncionario(@PathVariable UUID idFuncionario);
+	
+	@PatchMapping(value = "/altera/{idFuncionario}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	void alteraFuncionario(@PathVariable UUID idFuncionario, @Valid @RequestBody alteraFuncionario alteraFuncionario);
 	
 }
---
+++ src/main/java/com/uzitec/clienteservico/funcionario/application/api/request/alteraFuncionario.java
@@ -0,0 +1,15 @@
+package com.uzitec.clienteservico.funcionario.application.api.request;
+
+import com.uzitec.clienteservico.funcionario.domain.Cargo;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+@Value
+public class alteraFuncionario {
+	@NotBlank(message = "Campo Obrigatório!")
+	private String nomeFuncionario;
+	private String cpf;
+	private String celular;
+	private Cargo cargo;
+}
