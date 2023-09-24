4b10955ab3eaba459cc8ed21eeea1e95c9eef8b7
unknown <zelto@hotmail.com>
2023-09-24 06:24:41 -0300

    implementa contrato da findById no contrato da api

--- src/main/java/com/uzitec/clienteservico/ordemServico/application/api/OrdemServicoAPI.java
+++ src/main/java/com/uzitec/clienteservico/ordemServico/application/api/OrdemServicoAPI.java
@@ -1,6 +1,7 @@
 package com.uzitec.clienteservico.ordemServico.application.api;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,5 +17,9 @@ public interface OrdemServicoAPI {
 
 	@PostMapping
 	@ResponseStatus(value = HttpStatus.CREATED)
-	OrdemServicoResponse postOrdemServico(@Valid @RequestBody OrdemServicoRequest ordemServicoRequest);
+	OrdemServicoResponse saveOrdemServico(@Valid @RequestBody OrdemServicoRequest ordemServicoRequest);
+
+	@GetMapping(value = "/{idOrdemServico")
+	@ResponseStatus(value = HttpStatus.OK)
+	OrdemServicoResponse findOrdemServicoById(@Valid Long idOrdemServico);
 }
