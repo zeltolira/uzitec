ce660d52d14548380b48fd59b7a5e8aeece6ae95
unknown <zelto@hotmail.com>
2023-10-08 05:27:57 -0300

    implementa patchOrdemServico no contrato da api e cria PatchOrdemServicoRequestt

--- src/main/java/com/uzitec/clienteservico/ordemServico/application/api/OrdemServicoAPI.java
+++ src/main/java/com/uzitec/clienteservico/ordemServico/application/api/OrdemServicoAPI.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uzitec.clienteservico.ordemServico.application.api.request.OrdemServicoRequest;
+import com.uzitec.clienteservico.ordemServico.application.api.request.PatchOrdemServicoRequest;
 import com.uzitec.clienteservico.ordemServico.application.api.response.OrdemServicoResponse;
 
 import jakarta.validation.Valid;
@@ -30,6 +33,10 @@ public interface OrdemServicoAPI {
 	@GetMapping(value = "/allOrdemServico")
 	@ResponseStatus(value = HttpStatus.OK)
 	List<OrdemServicoResponse> findAllOrdemServico();
+	
+	@PatchMapping(value = "/{idOrdemServico}")
+	@ResponseStatus(value = HttpStatus.OK)
+	void patchOrdemServico (@PathVariable Long idOrdemServico, @Valid @RequestBody PatchOrdemServicoRequest patchOrdemServicoRequest);	
 	
 	
 }
---
+++ src/main/java/com/uzitec/clienteservico/ordemServico/application/api/request/PatchOrdemServicoRequest.java
@@ -0,0 +1,15 @@
+package com.uzitec.clienteservico.ordemServico.application.api.request;
+
+import com.uzitec.clienteservico.orcamento.domain.Marca;
+import com.uzitec.clienteservico.orcamento.domain.ServicoAExecutar;
+import com.uzitec.clienteservico.orcamento.domain.TipoProduto;
+
+import lombok.Value;
+
+@Value
+public class PatchOrdemServicoRequest {
+	private TipoProduto tipoProduto;
+	private Marca tipoMarca;
+	private ServicoAExecutar servicoAExecutar;
+	private String observacao;
+}
