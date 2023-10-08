a1c99ef5518e0c236c1b4c204099cab5d4e07637
unknown <zelto@hotmail.com>
2023-10-08 07:45:10 -0300

    cria metodo stream no ServicoResponse

--- src/main/java/com/uzitec/clienteservico/Servico/application/api/response/ServicoResponse.java
+++ src/main/java/com/uzitec/clienteservico/Servico/application/api/response/ServicoResponse.java
@@ -1,6 +1,8 @@
 package com.uzitec.clienteservico.Servico.application.api.response;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.uzitec.clienteservico.Servico.domain.Servico;
 import com.uzitec.clienteservico.orcamento.domain.ServicoAExecutar;
@@ -17,6 +19,12 @@ public class ServicoResponse {
 		this.idServico = servico.getIdServico();
 		this.servicoExecutado = servico.getServicoExecutado();
 		this.ValorServico = servico.getValorServico();
+	}
+
+	public static List<ServicoResponse> converte(List<Servico> servicos) {
+		return servicos.stream()
+				.map(ServicoResponse::new)
+				.collect(Collectors.toList());
 	}
 
 }
